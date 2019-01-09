610ff5eabde1c1948986a704b2322c9cb22142ee
jonroger3 <jonroger3@gmail.com>
2019-01-09 16:41:23 +0000

    Done.

--- loops.js
+++ loops.js
@@ -9,3 +9,18 @@ for (let i=0; i<t; i++){
 return [array, t];
 }
 
+function forLoop(array){
+  
+  
+      const [array, t] = makeArray();
+      const strangeArray = forLoop(array);
+      const testArray = strangeArray.slice(array.length);
+
+      let first = "I am 1 strange loop.";
+      let rest = "I am 24 strange loops.";
+      
+        
+      }
+      
+      
+      
