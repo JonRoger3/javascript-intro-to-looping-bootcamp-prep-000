a01e902f708eaa3998d3408a2caa20ba5dddaf12
jonroger3 <jonroger3@gmail.com>
2019-03-03 22:32:32 +0000

    Automatically backed up by Learn

--- loops.js
+++ loops.js
@@ -1,23 +1,10 @@
-function makeArray(){
-  const array = [];
-  const t = 10;
-
-for (let i=0; i<t; i++){
-  array.push("I am a strange loop");
-}
-
-return [array, t];
-}
-
-function forLoop(array){
+function forLoop(i){
+  for (let i = 0; i < 25; i++);
+  if (i===0){
+    console.log('I am 1 strange loop.');
+  }
   
-  
-      const [array, t] = makeArray();
-      const strangeArray = forLoop(array);
-      const testArray = strangeArray.slice(array.length);
-
-      let first = "I am 1 strange loop.";
-      let rest = "I am 24 strange loops.";
-      
-        
-      }
+  else{
+    console.log('I am ' +i+ ' strange loops');
+  }
+}
